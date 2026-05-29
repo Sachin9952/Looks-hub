44d255ea66e806deb21397f833a5f905c3b04963
sachin <sachinsinghakhawat9952@gmail.com>
2026-05-30 00:56:04 +0530

    chore: allow vercel client origin in CORS policy

--- server/src/app.js
+++ server/src/app.js
@@ -20,6 +20,7 @@ const app = express()
 // Middleware
 const allowedOrigins = [
   process.env.CLIENT_URL,
+  'https://looks-hub.vercel.app',
   'http://localhost:5173',
   'http://localhost:8080',
   'http://127.0.0.1:5173',
